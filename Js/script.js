db7c0e6ad51edd68481e5eb5e043c5f9d594fb58
aluno etec <etec@embu.com.br>
2024-08-22 22:38:21 -0300

    integrando operadores logicos IF/ELSE

--- Js/script.js
+++ Js/script.js
@@ -4,8 +4,9 @@ const idade = prompt("por favor,informe a sua idade:")
 const local = prompt("por favor, digite sua cidade natal");
 
 alert(`Meu nome é ${nome} e tenho ${idade} anos. Moro em ${local} desde crinça`);*/
-const n1 = parseInt (prompt("Informe um valor:"));
+/*const n1 = parseInt (prompt("Informe um valor:"));
 const n2 = parseInt (prompt("informe outro valor:"));
+const n3 = parseInt (prompt) 
 const result1 = n1+n2;
 alert (`a soma dos valores é: ${result1}`);
 const result2 = n1-n2;
@@ -19,6 +20,30 @@ alert (`a exponenciação dos valores é ${result5}`);
 const result6 = n1%n2;
 alert (`o resto da divisão dos valores é ${result6}`);
 const result = document.getElementById("result1");
-result.innerText = result1;
+result.innerText = result1;*/
+
+/*if "sempre vai ser primeiro" Elseif "será os intermediarios" Else "sempre vai ser o Ultimo"*/
+const n1 = parseInt (prompt("Informe o 1º número:"));
+const n2 = parseInt (prompt("Informe o 2º número:"));
+const n3 = parseInt (prompt("Informe o 3º número:"));
+
+
+/*if(n1 == n2 && n2 ==n3){
+    alert("Todos os números são iguais");
+}
+
+else {
+    alert("Existem números diferentes");
+} 
+*/
+
+if(n1>n2 || n2 >n3 || n3 >n1 || n2>n1){
+    alert($(Result));
+}
+else if(asdsada) {
+
+}
+
+
 
 
